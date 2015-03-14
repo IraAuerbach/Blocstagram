b86b122d8cd2167668a455ae895080af782abd98
Ira Auerbach <auerbach.ira@gmail.com>
2015-03-14 14:12:51 -0400

    table view controller implemented

---
+++ Blocstagram/ImagesTableViewController.h
@@ -0,0 +1,15 @@
+//
+//  ImagesTableViewController.h
+//  Blocstagram
+//
+//  Created by Ira Auerbach on 3/14/15.
+//  Copyright (c) 2015 Ira Auerbach. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ImagesTableViewController : UITableViewController
+
+@property (nonatomic, strong) NSMutableArray *images;
+
+@end
